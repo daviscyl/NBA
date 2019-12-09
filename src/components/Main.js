2cb2faac095889d393ad101508fdce29b727cefb
Davis Chan <davischan@MacBook-Pro.hsd1.ca.comcast.net>
2019-12-08 16:18:05 -0800

    +search bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { DataViewContainer } from "./DataViewContainer";
 import nba from "nba";
 import { Profile } from "./Profile";
+import { SearchBar } from "./SearchBar";
 import { DEFAULT_PLAYER_INFO } from "../constants";
 
 export class Main extends React.Component {
@@ -25,13 +26,10 @@ export class Main extends React.Component {
       });
   };
 
-  handleSelectPlayer = playerName => {
-    this.loadPlayerInfo(playerName);
-  };
-
   render() {
     return (
       <div className="main">
+        <SearchBar loadPlayerInfo={this.loadPlayerInfo} />
         <div className="player">
           <Profile playerInfo={this.state.playerInfo} />
           <DataViewContainer playerId={this.state.playerInfo.playerId} />
